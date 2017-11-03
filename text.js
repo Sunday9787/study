17c0849255ed9c57cf3fc24c28b715de0e7003ff
Edward <809537981@qq.com>
2017-11-03 18:09:10 +0800

    日常

--- text.js
+++ text.js
@@ -1,4 +1,48 @@
 // for(let i = 0; i < 20; i++) {
 //     console.log(i);
 // }
-console.log(parseInt(20*Math.random()));
+// console.log(parseInt(20*Math.random()));
+
+var obj = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 1,
+    e: {
+      f: 1,
+      g: {
+        h: 1,
+        i: {
+          j: 1
+        }
+      }
+    }
+  }
+}
+
+// function mmp(obj) {
+//   for (var i in obj) {
+//     if (typeof obj[i] !== 'object') {
+//       // console.log(i+'    '+obj[i]);
+//       console.log(i);
+//     }
+//     if(typeof obj[i] === 'object') {
+//       mmp(obj[i]);
+//     }
+//   }
+// }
+// mmp(obj);
+walk(obj, []);
+
+function walk(obj, prefix) {
+  Object.keys(obj).forEach((it) => {
+    const x = obj[it];
+    prefix.push(it);
+
+    if (typeof x === 'object') {
+      return walk(x, prefix);
+    }
+
+    console.log(prefix.join('.'), x);
+  });
+}
