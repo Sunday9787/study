6fcbbb702931deee89e0b5a0e1dfc3b3e76c4ac9
Edward <809537981@qq.com>
2018-03-12 17:37:02 +0800

    TS

---
+++ ts/index.ts
@@ -0,0 +1,11 @@
+function greeter(person: boolean) {
+  return 'Hello, ' + person;
+}
+
+let user = '😁';
+
+let isVip = false;
+
+const names: string[] = ['ok'];
+
+document.body.innerHTML = greeter(isVip);
---
+++ ts/js_output/index.js
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return 'Hello, ' + person;
+}
+var user = '😁';
+var isVip = false;
+var names = ['ok'];
+document.body.innerHTML = greeter(isVip);
